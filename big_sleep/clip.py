e26a6100c2d87047205b71134e5ef29500a37dfc
kateiyas <mue.katja@gmail.com>
2022-01-20 16:24:41 +0100

    add dxgb

--- big_sleep/clip.py
+++ big_sleep/clip.py
@@ -538,6 +538,7 @@ class CLIP(nn.Module):
     def encode_spose(self, sposevec, spose_to_clip_model, modeltype='xgboost'):
         if modeltype == 'xgboost':
             clip_emb = torch.tensor( spose_to_clip_model.predict( sposevec.cpu().numpy().reshape(1, -1) ), device='cuda:0' )
+            print("encode_spose_called")
         elif modeltype == 'linear':
             clip_emb = torch.matmul(sposevec, spose_to_clip_model)
         return clip_emb
