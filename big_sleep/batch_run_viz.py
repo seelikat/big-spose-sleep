fd0ae7e23b9c4464ae8bff9706c88744c8cf9def
kateiyas <mue.katja@gmail.com>
2022-02-17 17:00:12 +0100

    continue gen

---
+++ big_sleep/batch_run_viz.py
@@ -0,0 +1,18 @@
+import os, glob
+os.environ['CUDA_VISIBLE_DEVICES']='0'
+import fire
+import random as rnd
+from big_spose_sleep import Imagine
+from pathlib import Path
+import sys
+
+
+if __name__=="__main__":
+
+    reps = 5
+    sposeprofiles = glob.glob( "./*profile.txt" )
+
+    for ri in range(1, reps): 
+        for sposeprofilefn in enumerate(sposeprofiles):
+        os.system( "/LOCAL/kamue/anaconda3/bin/python start_test_run.py " + sposeprofilefn )  # does wait for completion
+            os.rename( sposeprofilefn+".png", sposeprofilefn + "_" + str(ri) + ".png" )
